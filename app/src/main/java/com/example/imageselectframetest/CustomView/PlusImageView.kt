f7a4e43fa4f9116ca8bb789c5fcec790200503f9
larsnoya <larsnoya@gmail.com>
2019-08-21 15:30:40 +0800

    20190821 調程式碼整

--- app/src/main/java/com/example/imageselectframetest/CustomView/PlusImageView.kt
+++ app/src/main/java/com/example/imageselectframetest/CustomView/PlusImageView.kt
@@ -23,23 +23,17 @@ class PlusImageView @JvmOverloads constructor(
     var imageViewWidth : Int = 0
     var imageViewHeight : Int = 0
     private val targetMatrix = Matrix()
-    private val savedMatrix = Matrix()
     var  dm : DisplayMetrics =context.getResources().getDisplayMetrics()
     enum class modeType(value : Int) {
         NONE (0),
         DRAG (1),
         ZOOM  ( 2)
     }
-    var mode = PlusImageView.modeType.NONE //模式
-    var start = PointF()
-    var mid = PointF()
-    var oldDist = 1f
 
     //圖片縮放合適大小
     fun initialImage(mWidth : Int,mHeight : Int) :Float{
         imageViewWidth=mWidth
         imageViewHeight=mHeight
-//        Log.d("Infor", "檢查 縮放倍率")
         val screenHeight = dm.heightPixels
         val screenWidth = dm.widthPixels
         targetMatrix.set(getImageMatrix())
@@ -50,10 +44,6 @@ class PlusImageView @JvmOverloads constructor(
         val height = rect.height()
         val width = rect.width()
         var max : Float=0F
-//        Log.d("Infor", "height"+mHeight.toString())
-//        Log.d("Infor", "screenHeight"+screenHeight.toString())
-//        Log.d("Infor", "width"+mWidth.toString())
-//        Log.d("Infor", "screenWidth"+screenWidth.toString())
         if(height >= screenHeight && width >= screenWidth){
             if(width/screenWidth>height/screenHeight) {
                 max=(screenWidth/width)
@@ -77,53 +67,5 @@ class PlusImageView @JvmOverloads constructor(
             setImageMatrix(targetMatrix)
         }
         return max
-    }
-    fun spacing( event: MotionEvent) : Float{
-        var x : Float= event.getX(0) - event.getX(1)
-        var y : Float= event.getY(0) - event.getY(1)
-        return Math.sqrt(x.toDouble() * x.toDouble() + y.toDouble() * y.toDouble()).toFloat()
-
-    }
-    fun midPoint( point :PointF,  event : MotionEvent) {
-        var x : Float = event.getX(0) +  event.getX(1)
-        var y : Float = event.getY(0) + event.getY(1)
-        point.set(x / 2, y / 2)
-    }
-    //圖片置中
-    fun center(vertical : Boolean, horizontal : Boolean){
-        val m = Matrix()
-        m.set(targetMatrix)
-//        val rect = RectF(0f, 0f, this.width.toFloat(), this.height.toFloat())
-        val rect = RectF(0f, 0f, imageViewWidth.toFloat(), imageViewHeight.toFloat())
-        m.mapRect(rect)
-        val height = rect.height()
-        val width = rect.width()
-
-        var deltaX = 0f
-        var deltaY = 0f
-
-        if (vertical) {
-            // 圖片小於螢幕大小，則置中顯示。
-            //大於螢幕，上方則留空白則往上移，下方留空白則往下移
-            val screenHeight = dm.heightPixels
-            if (height < screenHeight) {
-                deltaY = (screenHeight - height) / 2 - rect.top
-            } else if (rect.top > 0) {
-                deltaY = -rect.top
-            } else if (rect.bottom < screenHeight) {
-                deltaY = this.getHeight() - rect.bottom
-            }
-        }
-        if (horizontal) {
-            val screenWidth = dm.widthPixels
-            if (width < screenWidth) {
-                deltaX = (screenWidth - width) / 2 - rect.left
-            } else if (rect.left > 0) {
-                deltaX = -rect.left
-            } else if (rect.right < screenWidth) {
-                deltaX = screenWidth - rect.right
-            }
-        }
-        targetMatrix.postTranslate(deltaX, deltaY)
     }
 }
--- app/src/main/java/com/example/imageselectframetest/MainActivity.kt
+++ app/src/main/java/com/example/imageselectframetest/MainActivity.kt
@@ -1,68 +1,24 @@
 package com.example.imageselectframetest
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.view.View
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.request.RequestOptions
-import android.graphics.*
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.Bitmap
-import android.util.DisplayMetrics
-import android.util.Log
-import android.view.ViewTreeObserver
-import android.widget.ImageView
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.imageselectframetest.CustomView.CropActivity
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.IOException
-import java.util.*
-
 
 class MainActivity : AppCompatActivity() {
-    private var imageUri: Uri? = null
-    val tmpFile = File(Environment.getExternalStorageDirectory(), "/DCIM/image.jpg")
-    var targetWidth =0
-    var targetHeight =0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Btn_Cut.setOnClickListener {
+        Btn_Select.setOnClickListener {
             var intent_crop=Intent(this,CropActivity::class.java)
             startActivityForResult(intent_crop, 1);
-//            var intent_Crop = Intent("com.android.camera.action.CROP")
-//            intent_Crop.setType("image/*")
-//            intent_Crop.putExtra("circleCrop", "true");
-//            intent_Crop.putExtra("outputX", 150);
-//            intent_Crop.putExtra("outputY", 150);
-//            intent_Crop.putExtra("scale", true)
-//            intent_Crop.putExtra("return-data", false)
-//            val outputFileUri = Uri.fromFile(tmpFile)
-//            intent_Crop.putExtra("output", outputFileUri)
-//            intent_Crop.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString())
-//            startActivityForResult(intent_Crop, 1);
-        }
-        Btn_Cut2.setOnClickListener {
-//            saveBitmap(cropBitmap1())
-//            var path = (Environment.getExternalStorageDirectory()).path + "/DCIM/image.jpg"
-//            val updateTime = System.currentTimeMillis().toString()
-//            var potions = RequestOptions()
-//            potions.skipMemoryCache(true)
-//            potions.diskCacheStrategy(DiskCacheStrategy.NONE)
-//            Glide.with(this)
-//                .load(path)
-//                .apply(RequestOptions.bitmapTransform(CircleCrop()))
-//                .apply(potions)
-//                .into(imageViewCut)
         }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -71,7 +27,6 @@ class MainActivity : AppCompatActivity() {
             1 -> {
                 if (resultCode == Activity.RESULT_OK) {
                     var path = (Environment.getExternalStorageDirectory()).path + "/DCIM/image.jpg"
-//                    Glide.with(this).load(path).apply(RequestOptions.bitmapTransform(CircleCrop())).into(imageViewCut)
                     var potions = RequestOptions()
                     potions.skipMemoryCache(true)
                     potions.diskCacheStrategy(DiskCacheStrategy.NONE)
@@ -83,48 +38,5 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-    }
-
-    private fun cropBitmap1(): Bitmap {
-        val bmp2 = (imageViewOrigin.getDrawable() as BitmapDrawable).bitmap
-        val bmOverlay = Bitmap.createBitmap(150, 150, Bitmap.Config.ARGB_8888)
-        val paint = Paint()
-        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
-        var rectArray = imageView2.getCutArr()
-        var srcRect = Rect(rectArray[0].toInt(), rectArray[1].toInt(), rectArray[2].toInt(), rectArray[3].toInt())
-        var targetRect = RectF(0F, 0F, 150F, 150F)
-        val canvas = Canvas(bmOverlay)
-//        canvas.drawBitmap(bmp2, 0F, 0F, null)
-        canvas.drawBitmap(bmp2, srcRect, targetRect, null)
-//        canvas.drawRect(rectArray[0], rectArray[1], rectArray[2], rectArray[3], paint)
-        return bmOverlay
-    }
-    fun saveBitmap(bitmap: Bitmap) {
-        val fOut: FileOutputStream
-        var path = (Environment.getExternalStorageDirectory()).path + "/DCIM/image.jpg"
-        try {
-//            val dir = File("/sdcard/demo/")
-//            if (!dir.exists()) {
-//                dir.mkdir()
-//            }
-
-//            val tmp = "/sdcard/demo/takepicture.jpg"
-            if (File(path).exists()) {
-                File(path).delete()
-            }
-            fOut = FileOutputStream(path)
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOut)
-
-            try {
-                fOut.flush()
-                fOut.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-        }
-
     }
 }
